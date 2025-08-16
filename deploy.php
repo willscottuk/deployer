f6a8b96880996075be722d47030fded9c0c9eed0
Will <willscottuk@users.noreply.github.com>
2025-08-16 17:29:08 +0100

    :rocket: Deploying!

---
+++ deploy.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Deployer;
+
+use Exception;
+
+require 'recipe/laravel.php';
+require 'contrib/php-fpm.php';
+require 'contrib/npm.php';
+require 'contrib/crontab.php';
+require 'contrib/sentry.php';
+
+// Config
+set('application', getenv('DEPLOYER_APP'));
+set('repository', getenv('DEPLPOYER_REPO'));
+set('php_fpm_version', '8.1');
+set('cleanup_use_sudo', true);
+set('keep_releases', 3);
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+add('crontab:jobs', [
+  '* * * * * cd {{release_path}} && {{bin/php}} artisan schedule:run >> /dev/null 2>&1',
+]);
+
+// Hosts
+
+host('prod')
+  ->set('remote_user', getenv('DEPLOYER_USER'))
+  ->set('hostname', getenv('DEPLOYER_HOSTNAME'))
+  ->set('deploy_path', getenv('DEPLOYER_PATH'))
+  ->set('bin/crontab', 'sudo /usr/bin/crontab')
+  ->set('bin/php', 'sudo docker exec -d -t -w {{release_path}} swag php')
+  ->set('bin/composer', 'sudo docker exec -t -w {{release_path}} swag /config/php/composer.phar');
+
+task('deploy', [
+  'version:prepare',
+  'deploy:prepare',
+  'composer:prepare',
+  'deploy:vendors',
+  'version:set',
+  'artisan:horizon:terminate',
+  'artisan:storage:link',
+  'artisan:view:cache',
+  'artisan:config:cache',
+  'artisan:migrate',
+  'npm:install',
+  'npm:run:prod',
+  'artisan:horizon',
+  'deploy:publish',
+]);
+
+after('deploy:vendors', 'deploy:version:prepare');
+task('deploy:version:prepare', function () {
+  run('sudo docker exec -t -w /srv/web/sites/spark/.dep/repo/ swag git config --global --add safe.directory /srv/web/sites/spark/.dep/repo');
+});
+
+task('composer:prepare', function () {
+  run('sudo docker exec -t -w {{release_path}} swag /config/php/composer.phar config http-basic.wire-elements-pro.composer.sh %secret%', secret: getenv('WIRE_SECRET'));
+});
+
+after('deploy:version:prepare', 'deploy:version:absorb');
+task('deploy:version:absorb', artisan('version:absorb'));
+after('deploy', 'deploy:sentry:once');
+
+task('version:prepare', function () {
+  $ver = runLocally('php artisan version:show --format=version-only --suppress-app-name');
+  $commit = substr(runLocally('git rev-parse --verify HEAD'), 0, 6);
+  set('sentry', [
+    'organization' => getenv('SENTRY_ORG'),
+    'projects' => [
+      getenv('DEPLOYER_PROJECT_ARRAY')
+    ],
+    'token' => getenv('SENTRY_TOKEN'),
+    'environment' => 'production',
+    'version' => $ver . '+' . $commit,
+    'version_prefix' => getenv('SENTRY_PREFIX'),
+    'sentry_server' => getenv('SENTRY_SERVER'),
+  ]);
+  set('version', getenv('SENTRY_PREFIX') . $ver . '+' . $commit);
+  writeln('<info>' . get('version') . '</info>');
+});
+
+task('version:set', function () {
+  $ver = get('version');
+  run("echo {$ver} > {{release_path}}/VERSION");
+});
+
+task('npm:run:prod', function () {
+  run('sudo docker exec -t -w {{release_path}} swag npm run build');
+});
+
+task('deploy:sentry:once', function () {
+  try {
+    invoke('deploy:sentry');
+  } catch (Exception $e) {
+    info('Unable to push release or deploy to Sentry : ' . $e->getMessage());
+  }
+})->once();
+
+after('deploy:failed', 'deploy:unlock');
+after('deploy:success', 'crontab:sync');
